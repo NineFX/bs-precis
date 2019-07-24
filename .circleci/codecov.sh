5c2e8a1a848f17508a73ef8bb80aeb49afd41576
Justin Baum <justin.baum@ninefx.com>
2019-07-24 14:42:02 -0400

    reduce test size for CI testing

--- .circleci/codecov.sh
+++ .circleci/codecov.sh
@@ -6,7 +6,7 @@ yarn jest
 EXITCODE=$?
 
 # Upload to CodeCov
-bash <(curl -s https://codecov.io/bash) -f ./coverage/coverage.json
+bash <(curl -s https://codecov.io/bash) -f ./coverage/coverage-final.json
 
 exit $EXITCODE
 popd
