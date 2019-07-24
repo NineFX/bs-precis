4f5e5a2bf4ed5aee0d2be920b8af531e98547060
Justin Baum <justin.baum@ninefx.com>
2019-07-24 14:35:41 -0400

    Add correct directory pop

--- .circleci/codecov.sh
+++ .circleci/codecov.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd -P)"
+
+pushd $SCRIPT_DIR/..
 yarn jest
 EXITCODE=$?
 
+bash<(curl -s https://codecov.io/bash)
 # Upload to CodeCov
-bash <(curl -s https://codecov.io/bash) -f coverage/coverage.json
+bash <(curl -s https://codecov.io/bash) -f ./coverage/coverage.json
 
 exit $EXITCODE
+popd
